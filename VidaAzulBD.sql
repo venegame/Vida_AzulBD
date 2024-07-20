43ae8d5d50af71f28ebf549ad0b7a5eff07282b4
Melany <>
2024-07-20 00:35:19 -0600

    CREATION OF TABLES USUARIOS, PROYECTOS Y ROLES.

--- VidaAzulBD.sql
+++ VidaAzulBD.sql
@@ -1,5 +1,26 @@
-This is a test;
+-- Crear tabla Usuario
+CREATE TABLE Usuarios (
+    id_usuario INT PRIMARY KEY,
+    nombre_usuario VARCHAR2(80) NOT NULL,
+    apellido_usuario VARCHAR2(80) NOT NULL,
+    correo_usuario VARCHAR2(80) NOT NULL,
+    contraseña_usuario VARCHAR2(15) NOT NULL,
+    id_rol INT);
+//FOREIGN KEY (id_rol) REFERENCES Rol(id_rol)
 
-hola this is another test;
+-- Crear tabla Proyecto
+CREATE TABLE Proyectos (
+    id_proyecto INT PRIMARY KEY,
+    id_usuario INT,
+    id_categoria INT,
+    nombre_proyecto VARCHAR2(80) NOT NULL,
+    detalle_proyecto VARCHAR2(200) NOT NULL,
+    estado_proyecto VARCHAR2(30) NOT NULL);
+//FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
+//FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria)
 
-Hola esto es un test de Carlitos;
+-- Crear tabla Rol
+CREATE TABLE Roles (
+    id_rol INT PRIMARY KEY,
+    nombre_rol VARCHAR2(50) NOT NULL);
+    
