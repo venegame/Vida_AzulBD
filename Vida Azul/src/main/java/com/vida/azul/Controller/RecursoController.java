18151d9280305f1179063c23cf88d0279637a6ba
venegame <melany.venegas2@gmail.com>
2024-08-26 00:30:26 -0600

    Filtro recursos fixed

--- Vida Azul/src/main/java/com/vida/azul/Controller/RecursoController.java
+++ Vida Azul/src/main/java/com/vida/azul/Controller/RecursoController.java
@@ -1,42 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.vida.azul.Controller;
 
 import com.vida.azul.Service.RecursoService;
 import com.vida.azul.domain.Recurso;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- *
- * @author Me
- */
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/recurso")
 public class RecursoController {
-    
+
     @Autowired
     private RecursoService recursoService;
 
     @GetMapping("/recursos")
-    public String mostrarListado(Model model) {
-        List<Recurso> recursos = recursoService.obtenerRecursos();
+    public String mostrarListado(
+            @RequestParam(value = "filtrocategoria", required = false) Integer filtrocategoria,
+            Model model) {
+        // Llamar al servicio para obtener recursos filtrados
+        List<Recurso> recursos = recursoService.obtenerRecursos(filtrocategoria);
         model.addAttribute("recursos", recursos);
+        model.addAttribute("filtrocategoria", filtrocategoria);
         return "recurso/recursos";
     }
-    
+
     @GetMapping("/detallerecurso/{id}")
     public String verDetalleRecurso(@PathVariable("id") int id, Model model) {
         Recurso recurso = recursoService.obtenerRecursoPorId(id);
         model.addAttribute("recurso", recurso);
         return "recurso/detallerecurso"; // Devolver solo el nombre de la vista sin parámetros
     }
-
 }
