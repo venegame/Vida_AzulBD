1060d09d3e851b075e8b9d2fa1ac0eaa171d82bc
venegame <melany.venegas2@gmail.com>
2024-08-25 23:09:52 -0600

    Pagina transportes html completada

---
+++ Vida Azul/src/main/java/com/vida/azul/Controller/TransporteController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.vida.azul.Controller;
+
+import com.vida.azul.Service.TransporteService;
+import com.vida.azul.domain.Transporte;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Me
+ */
+@Controller
+@RequestMapping("/transporte")
+public class TransporteController {
+    
+    @Autowired
+    private TransporteService transporteService;
+    
+    @GetMapping("/transportes")
+    public String mostrarListado(Model model) {
+        List<Transporte> transportes = transporteService.obtenerTransportes();
+        model.addAttribute("transportes", transportes);
+        return "transporte/transportes";
+    }
+    
+}
